ca4d75c3b06a5e6b99e55406e36f7a8e307091be
sophie <soph_lew@hotmail.co.uk>
2022-10-24 17:56:14 +0100

    updated index.js server with connections

--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+require('dotenv').config()
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}))         // setup bodyparser to properly send requests
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))   // setup bodyparser to properly send requests
+app.use(cors())
+
+const CONNECTION_URL = `mongodb+srv://memories:${process.env.CONNECTION_PASSWORD}@cluster0.dspkyot.mongodb.net/?retryWrites=true&w=majority`
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app,listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false)
