a0c867df05eefed881e5a1e9feaef2b8baa9c417
sophie <soph_lew@hotmail.co.uk>
2022-10-25 15:37:11 +0100

    updated App in client with dispatch, and actions>posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react'; // useEffect will be the component we update
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux' // dispatch an action
 
+import { getPosts } from './actions/posts'
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
 import memories from './images/memories-brain.gif'
@@ -8,6 +10,11 @@ import useStyles from './styles.js'
 
 const App = () => { // functional component
   const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+      dispatch(getPosts)
+  }, [dispatch])
   return (
     <Container maxwidth = "lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
---
+++ client/src/actions/posts.js
@@ -0,0 +1,4 @@
+// now have to make use of the index.js file in api
+import * as api from '../api' // import everything from actions as api
+
+// Action creators
